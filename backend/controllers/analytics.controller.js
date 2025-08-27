fffc040b04860786f34430df911c1951b3e1bb41
Rohit Vishwakarma <vishro1221@gmail.com>
2025-08-27 18:09:49 +0530

    Update analytics.controller.js

--- backend/controllers/analytics.controller.js
+++ backend/controllers/analytics.controller.js
@@ -2,29 +2,34 @@ import Order from "../models/order.model.js";
 import Product from "../models/product.model.js";
 import User from "../models/user.model.js";
 
-export const getAnalyticsData = async () => {
-	const totalUsers = await User.countDocuments();
-	const totalProducts = await Product.countDocuments();
+export const getAnalyticsData = async (req, res) => {
+	try {
+		const totalUsers = await User.countDocuments();
+		const totalProducts = await Product.countDocuments();
 
-	const salesData = await Order.aggregate([
-		{
-			$group: {
-				_id: null, // it groups all documents together,
-				totalSales: { $sum: 1 },
-				totalRevenue: { $sum: "$totalAmount" },
+		const salesData = await Order.aggregate([
+			{
+				$group: {
+					_id: null,
+					totalSales: { $sum: 1 },
+					totalRevenue: { $sum: "$totalAmount" },
+				},
 			},
-		},
-	]);
+		]);
 
-	const { totalSales, totalRevenue } = salesData[0] || { totalSales: 0, totalRevenue: 0 };
+		const { totalSales, totalRevenue } = salesData[0] || { totalSales: 0, totalRevenue: 0 };
 
-	return {
-		users: totalUsers,
-		products: totalProducts,
-		totalSales,
-		totalRevenue,
-	};
+		res.json({
+			users: totalUsers,
+			products: totalProducts,
+			totalSales,
+			totalRevenue,
+		});
+	} catch (err) {
+		res.status(500).json({ message: "Server error", error: err.message });
+	}
 };
+
 
 export const getDailySalesData = async (startDate, endDate) => {
 	try {
